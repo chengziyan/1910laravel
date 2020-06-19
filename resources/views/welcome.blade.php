11cbbcc244c30651a111f4c87732d22dd85bc4c7
wangyanrui <3376155385@qq.com>
2020-06-19 16:00:07 +0800

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    hi sweet
                 </div>
 
                 <div class="links">
